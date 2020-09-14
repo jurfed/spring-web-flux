9b197727ebf188de435ff1838b0769de104e1793
Juriy <jurfed@inbox.ru>
2020-09-14 13:27:27 +0300

    first commit

---
+++ src/test/java/ru/jurfed/springwebflux/SpringWebFluxApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.jurfed.springwebflux;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebFluxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
